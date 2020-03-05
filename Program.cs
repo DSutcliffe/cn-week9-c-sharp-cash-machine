9ec7d38a51ce2e92f459a8701a3a7a249307dfba
Daniel Sutcliffe <dannysutcliffe@hotmail.com>
2020-03-05 09:09:11 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace cn_week9_c_sharp_cash_machine
+{
+    class Program
+    {
+        void withdrawal(int bankBalance, int withdrawalAmount)
+        {
+            bankBalance -= withdrawalAmount;
+            Console.WriteLine("My new balance is: £{0}", bankBalance);
+        }
+        void checkPin(string userInput, string pinNumber)
+        {
+            if(userInput == pinNumber)
+            {
+                Console.WriteLine("PIN OK");
+            }
+            else
+            {
+                Console.WriteLine("PIN INCORRECT");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program cashMachine = new Program();
+            cashMachine.withdrawal(500, 30);
+
+            // taking user input
+            string pinNumber = "1000";
+            string userInput;
+            Console.Write("Please enter your input: ");
+            userInput = Console.ReadLine();
+            Console.WriteLine(userInput);
+
+            cashMachine.checkPin(userInput, pinNumber);
+        }
+    }
+}
